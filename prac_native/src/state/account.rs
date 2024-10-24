7b33da926bc7d4b357c24d5a88f1f9954c9efdcf
harshana15 <harshana555prajapati@gmail.com>
2024-10-24 11:01:43 +0530

    native custom_data_instruction added.

--- prac_native/src/state/account.rs
+++
@@ -1,8 +0,0 @@
-use borsh::{ BorshDeserialize, BorshSerialize};
-
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct RentVault {}
-
-impl RentVault {
-    pub const SEED_PREFIX: &'static str = "VAULT";
-}
